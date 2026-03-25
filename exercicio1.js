2af60ac10d22f235b0b8c3cbc1fe529b4b0c783a
Juliano Costa <juhc32@hotmail.com>
2026-03-24 23:35:38 -0300

    chore: adiciona estrutura inicial dos arquivos js com comentários

---
+++ exercicio1.js
@@ -0,0 +1,14 @@
+/******************************************************************************
+Curso: Tecnologia em Análise e Desenvolvimento de Sistemas
+Disciplina: Programação Front-End
+Professor: José Carlos Flores
+Turma: ADS3B
+Componentes: 
+             25291244-2 -  Juliano Henrique Costa
+             25260842-2 -  Pedro Rocha Reis
+             25000914-2 -  Lucas de Freitas Ferreira de Oliveira
+             25363470-2 -  Fernando Araujo Alvim
+             25363563-2 -  Sara Kerstin Miguel
+Data: 10 de abril de 2026
+Descritivo: Crie um programa que verifique se um número é par ou ímpar.
+*******************************************************************************/
---
+++ exercicio5.js
@@ -0,0 +1,14 @@
+/******************************************************************************
+Curso: Tecnologia em Análise e Desenvolvimento de Sistemas
+Disciplina: Programação Front-End
+Professor: José Carlos Flores
+Turma: ADS3B
+Componentes: 
+             25291244-2 -  Juliano Henrique Costa
+             25260842-2 -  Pedro Rocha Reis
+             25000914-2 -  Lucas de Freitas Ferreira de Oliveira
+             25363470-2 -  Fernando Araujo Alvim
+             25363563-2 -  Sara Kerstin Miguel
+Data: 10 de abril de 2026
+Descritivo: Verifique se um ano é bissexto (divisível por 4, mas não por 100, exceto se for divisível por 400)
+*******************************************************************************/
